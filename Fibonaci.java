12591a3f9cb4c454004fd11ff356e2b437142b9f
Aditya7verma <129338439+Aditya7verma@users.noreply.github.com>
2023-07-09 01:05:48 +0530

    Add files via upload

---
+++ Fibonaci.java
@@ -0,0 +1,25 @@
+package Lac_4;
+
+import java.util.Scanner;
+
+public class Fibonaci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+
+        int a=0;
+        int b=1;
+        int i=1;
+
+        while (i<=n) {
+            int c =a + b;
+            a = b;
+            b = c;
+            i++;
+            System.out.print(a + " ");
+        }
+        System.out.println();
+
+        System.out.print(a);
+    }
+}
